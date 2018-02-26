b64a7087237f91e29d71035a66075d1f30d9cb5b
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-26 18:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,19 +28,11 @@ var sortedRobots = robots.map()
 
 
 
-// function map(collection, callback) {
-//     const result = [];
-//
-//     for (let i = 0; i < collection.length; i++) {
-//       const element = collection[i];
-//       result.push(callback(element, i, collection));
-//     }
-//
-//     return result;
-//   }
-//
-// const numbers = [1, 2, 3];
-// const doubledNumbers = map(numbers, function (number) {
-//  return number * 2;
+// const transformedAutobotsWithMap = autobots.map(function (autobot) {
+//   return Object.assign({}, autobot, {
+//     strength: autobot.strength * 2,
+//     isTransformed: true,
+//   });
 // });
-// console.log(doubledNumbers);
+//
+// console.log(transformedAutobotsWithMap);
